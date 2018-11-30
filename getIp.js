1fd6f9c98197c515ed6597734975317cad28a8b9
unknown <weblixin@163.com>
2018-11-30 15:46:24 +0800

    ok

---
+++ getIp.js
@@ -0,0 +1,17 @@
+const OS = require('os')
+
+const HOST = (function () {
+    var interfaces = OS.networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+})()
+
+module.exports = HOST;
+
