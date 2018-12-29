6e175c7eb7c8bc2ff1e0bc2a9389d6b083f8cb80
随手流年 <32237197+weblixin@users.noreply.github.com>
2018-12-29 15:39:06 +0800

    Update eruda.js

--- eruda.js
+++ eruda.js
@@ -2,9 +2,9 @@
 
 (function () {
     var script = document.createElement('script');
-    script.src = "http://eruda.liriliri.io/eruda.min.js";
+    script.src="//cdn.jsdelivr.net/npm/eruda";
     document.body.appendChild(script);
     script.onload = function () {
         eruda.init()
     }
-})()
+})();
